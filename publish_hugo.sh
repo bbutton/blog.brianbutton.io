31e730d5d60ded81ed4ae3e348a174185b25d012
Brian Button <bbutton@agilestl.com>
2019-08-10 12:31:40 -0400

    final version for now. considering adding a force option

--- publish_hugo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-hugo --contentDir posts --destination ../public --cleanDestinationDir --minify --templateMetrics --templateMetricsHints
-
--- scripts/publish_blog.sh
+++ scripts/publish_blog.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-hugo --contentDir=posts --verbose --verboseLog --destination public --cleanDestinationDir --minify --templateMetrics --templateMetricsHints
+if [ $# -ne 2 ]; then
+	echo "usage: $0 <contentDir> <destinationDir>" >&2
+	exit 1
+fi
 
-exit $?
-
+git remote -v update 2>&1 | grep "up to date" | grep -q master
+if [ $? -ne 0 ]; then
+	echo "Changed detexted, updating blog source before publishing"
+	git pull
+	hugo --verbose --verboseLog --contentDir $1 --destination $22 --cleanDestinationDir --minify --templateMetrics --templateMetricsHints
+else
+	echo "No changes detected, blog not updated"
+fi
