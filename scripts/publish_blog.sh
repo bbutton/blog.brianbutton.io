e14ea9ad3738ec4a8ff768ff6c510a423bea4af5
Brian Button <bbutton@agilestl.com>
2019-08-11 15:48:15 -0500

    Added ability to force a publish to happen, regardless if underlying content has changed

--- scripts/publish_blog.sh
+++ scripts/publish_blog.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "usage: $0 <contentDir> <destinationDir>" >&2
+FORCE=0
+PUBLISH_NEEDED=0
+
+if [ $# -lt 2 -o $# -gt 3 ]
+	echo "usage: $0 [--force] <contentDir> <destinationDir>" >&2
 	exit 1
 fi
 
-git remote -v update 2>&1 | grep "up to date" | grep -q master
-if [ $? -ne 0 ]; then
-	echo "Changed detexted, updating blog source before publishing"
-	git pull
-	hugo --verbose --verboseLog --contentDir $1 --destination $2 --cleanDestinationDir --minify --templateMetrics --templateMetricsHints
-else
-	echo "No changes detected, blog not updated"
+if [ "$1" == "force" ]; then 
+	FORCE=1
+	PUBLISH_NEEDED=1
+	shift
 fi
+
+if [ $FORCE -eq 0 ]; then 
+	git remote -v update 2>&1 | grep "up to date" | grep -q master
+	if [ $? -ne 0 ]; then
+		echo "Changed detected, updating blog source before publishing"
+		git pull
+		PUBLISH_NEEDED=1
+	else
+		echo "No changes detected, blog not updated"
+	fi
+fi
+
+if [ $PUBLISH_NEEDED -eq 1 ]; then
+	echo "Publishing new blog to $2"
+	hugo --verbose --verboseLog --contentDir $1 --destination $2 --cleanDestinationDir --minify --templateMetrics --templateMetricsHints
+fi
+
+exit 0
